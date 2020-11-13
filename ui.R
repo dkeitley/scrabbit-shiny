d891c6125043d5cddfaf62c0135b96b4cab8c6d8
Daniel Keitley <dkeitley@gmail.com>
2020-11-13 13:47:48 +0000

    Figured out how to add IGV into Shiny.

--- ui.R
+++ ui.R
@@ -1,24 +1,25 @@
 
 library(shiny)
 
+setwd("G:/My Drive/Postgrad/PhD/Projects/rabbit-gastrulation-shiny/")
 
 big_plot_width = 9 * 1.5
 big_plot_height = 5 * 1.5
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-    
-    
+    tags$head(tags$script(src="igv.min.js")),
+   
     sidebarLayout(
         sidebarPanel(
             id="sidebar",
             # style = "position:fixed;width:15%;",
             width = 2,
             h3("Plot options"),
-            
-            
+
+
             #SIDEBAR INPUTS
-            
+
             selectInput(
                 "coord_type",
                 "Projection type",
@@ -33,11 +34,11 @@ shinyUI(fluidPage(
                     "All timepoints" = "all",
                     "GD7" = "gd7" ,
                     "GD8" ="gd8",
-                    "GD9" = "gd9" 
+                    "GD9" = "gd9"
                 ),
                 selected = "all"
             ),
-            
+
             selectInput(
                 "colourby",
                 "Plot colour",
@@ -50,7 +51,7 @@ shinyUI(fluidPage(
                 selected = "celltype"
             ),
             selectizeInput("gene", "Gene", choices = NULL, selected = 26600),
-            
+
             #checkboxInput("numbers", "Annotate clusters in plot"),
             checkboxInput("subset", "Subset cells (faster plots when many points present)")
             # selectInput(
@@ -62,50 +63,60 @@ shinyUI(fluidPage(
             #downloadButton("downloadGeneTSNE", label = "Gene expr. vis"),
             #downloadButton("downloadGeneViolin", label = "Gene expr. violins")
         ),
-    
-    
-    
+
+
+
         mainPanel(
             id="main",
             width=10,
             titlePanel("Rabbit Gastrulation Atlas"),
-        
-        
-        
+
+
+
             tabsetPanel(
                 id = "tabs",
-                
+
+                tabPanel(
+                    "Genome annotation",
+                    id = "genome"
+
+                ),
+
                 tabPanel(
                     "Dataset overview",
-                    id = "overiew",
-                    plotOutput("data"),
-                    
+                    id = "overiew"
+                    #plotOutput("data"),
+
                 ),
-                
+
                 tabPanel(
                     "Cell type markers",
                     id = "markers"
                     #plotOutput("data", width = big_plot_width, height = big_plot_height),
-                    
+
                 ),
-                
+
                 tabPanel(
                     "Compare cell types",
                     id = "celltype"
                     #plotOutput("data", width = big_plot_width, height = big_plot_height),
-                    
+
                 ),
-                
-                
+
+
                 tabPanel(
                     "Compare trajectories",
                     id = "trajectory"
                     #plotOutput("data", width = big_plot_width, height = big_plot_height),
-                    
+
                 )
-            )
+            ),
+            HTML('  <div id="igvDiv" style="padding-top: 10px;padding-bottom: 10px; border:1px solid lightgray"></div>'),
+            tags$script(src = "igv.js")
         )
-        
+
+
     )
+    
     
 ))
